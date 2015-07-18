cd6057a27af0516885dec974b26275568d84e0e5
xinnix <xinnix2000@gmail.com>
2015-07-18 08:39:49 +0800

    construct msg

--- src/tackerUtil/MsgParser.java
+++ src/tackerUtil/MsgParser.java
@@ -1,15 +1,37 @@
 package tackerUtil;
 
 public class MsgParser {
-	public String msgheader;
+	
+	public static short MSG_SEND = 0x01;
+	public static short MSG_RECV = 0x02;
+	
+	
+	public String msgHead;
 	public short msgLength;
-	public String termID;
+	public String msgTermID;
 	public short msgType;
-	public short mksgArg;
+	public short msgArg;
 	public String msgContent;
+	public short msgReply;
 	public short msgCrc;
-	public short msgender;	
+	public short msgEnd;	
 	public byte[] msgByteBuf;
+	
+	MsgParser(int type,String termID,short msgType,short msgArg){
+		if (type==MsgParser.MSG_SEND){
+			this.msgHead="$$";
+			this.msgLength = 0x0011; //暂时固定长度
+			this.msgTermID = termID;
+			this.msgType = msgType;
+			this.msgArg = msgArg;
+			this.msgContent = "";
+			this.msgEnd = 0x0d0a;
+		}
+		
+		
+	
+		
+	}
 	
 	
 }
